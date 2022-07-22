5fe74fcb509e84d75f9a7133118a41d7b0579127
GOBINDA CHANDRA DAS <90102173+Gobindactg@users.noreply.github.com>
2022-07-22 09:20:03 +0600

    update action bar

--- resources/views/Admin/Pages/manageBlog.blade.php
+++ resources/views/Admin/Pages/manageBlog.blade.php
@@ -1,17 +1,30 @@
 @extends('Admin.Layout.MasterLayout')
 @section('content')
-<h1>Manage Slider</h1>
-<a href="{{route('blog')}}" class="btn btn-info mt-2 mb-2">Add Blog</a>
-<table class="table table-bordered">
+<link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.12.1/datatables.min.css" />
+<div style="border: 2px solid teal; margin:5px; padding:5px;" class="overflow-auto">
+<div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <a href="#" class="btn btn-info mb-2">Manage Blog</a>
+            </div>
+            <div class="col-md-6">
+                <a href="{{route('blog')}}" class="btn btn-primary mb-2" style="float: right;"><i style="font-size:15px" class="fa">&#xf067;</i> Add Blog</a>
+            </div>
+        </div>
+
+    </div>
+
+
+<table class="table table-bordered" id="table1">
     <thead>
-        <tr>
-            <th>S.L</th>
-            <th>Title</th>
-            <th>Description</th>
-            <th>Image</th>
-            <th>Status</th>
-            <th>Category</th>
-            <th>Action</th>
+        <tr style="text-align:center ;">
+            <th style="width: 5%;">S.L</th>
+            <th style="width: 25%;">Title</th>
+            <th style="width: 40%;">Description</th>
+            <th style="width: 5%;">Image</th>
+            <th style="width: 5%;">Status</th>
+            <th style="width: 10%;">Category</th>
+            <th style="width: 10%;">Action</th>
         </tr>
     </thead>
     <tbody>
@@ -31,38 +44,61 @@
             </td>
             <td> {{$blog->Category->name}}</td>
             <td>
-                <a href="javascript:void(0)" class="btn btn-info" onclick="updateBlog({{ $blog->id }})">Edit</a>
-                <a href="javascript:void(0)" class="btn btn-danger" onclick="deleteBlog({{ $blog->id }})">delete</a>
-                
+                <div class="dropdown">
+                    <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                        Action
+                    </button>
+                    <div class="dropdown-menu pl-1 mt-2" aria-labelledby="dropdownMenuButton">
+                        <a href="javascript:void(0)" class="btn btn-info" style="width: 47%;" onclick="updateBlog({{ $blog->id }})">Edit</a>
+                        <a href="javascript:void(0)" class="btn btn-danger"style="width: 47%;"  onclick="deleteBlog({{ $blog->id }})">delete</a>
+                    </div>
+                </div>
+
+
             </td>
         </tr>
         @endforeach
     </tbody>
 </table>
-
-<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.js" integrity="sha512-n/4gHW3atM3QqRcbCn6ewmpxcLAHGaDjpEBu4xZd47N0W2oQ+6q7oc3PXstrJYXcbNU1OHdQ1T7pAP+gi5Yu8g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
+<script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.js" integrity="sha512-n/4gHW3atM3QqRcbCn6ewmpxcLAHGaDjpEBu4xZd47N0W2oQ+6q7oc3PXstrJYXcbNU1OHdQ1T7pAP+gi5Yu8g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script> -->
 <script>
-      $.ajaxSetup({
+    $.ajaxSetup({
         headers: {
             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
         }
     });
-        function deleteBlog(id) {
-            if (confirm("Do You want to delete this Customer")) {
 
-                let _token = $("input[name=_token]").val();
-                $.ajax({
-                    url: 'http://localhost/internship/internShip/public/deleteBlog/' + id,
-                    type: 'GET',
-                    data: {
-                        _token: _token
-                    },
-                    success: function(response) {
-                        window.location.reload();
+    function deleteBlog(id) {
+        if (confirm("Do You want to delete this Customer")) {
 
-                    }
-                });
-            }
+            let _token = $("input[name=_token]").val();
+            $.ajax({
+                url: 'http://localhost/internship/internShip/public/deleteBlog/' + id,
+                type: 'GET',
+                data: {
+                    _token: _token
+                },
+                success: function(response) {
+                    window.location.reload();
+
+                }
+            });
         }
-    </script>
+    }
+</script>
+
+
+<script>
+    $(document).ready(function() {
+        $('#table1').DataTable();
+    });
+</script>
+<script>
+    $('#table1').DataTable({
+        "pageLength": 10
+    });
+</script>
 @endsection
--- resources/views/Admin/Pages/manageLogo.blade.php
+++ resources/views/Admin/Pages/manageLogo.blade.php
@@ -2,59 +2,67 @@
 @section('content')
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/v/bs5/dt-1.12.1/datatables.min.css" />
 <style>
-    .custom{
+    .custom {
         margin-bottom: 30px;
         margin-bottom: 30px;
         margin-right: 30px;
         float: right;
     }
 </style>
-<div style="border: 2px solid teal; margin:5px; padding:5px">
-<div class="container">
-    <div class="row">
-        <div class="col-md-6">
-            <a href="#" class="btn btn-info mb-2">ManageLogo</a>
+<div style="border: 2px solid teal; margin:5px; padding:5px; ;">
+    <div class="container">
+        <div class="row">
+            <div class="col-md-6">
+                <a href="#" class="btn btn-info mb-2">ManageLogo</a>
+            </div>
+            <div class="col-md-6">
+                <a href="{{route('logo')}}" class="btn btn-info mb-2" style="float: right;"><i style="font-size:15px" class="fa">&#xf067;</i> Add Logo</a>
+            </div>
         </div>
-        <div class="col-md-6" >
-            <a href="{{route('logo')}}"  class="btn btn-info mb-2" style="float: right;"><i style="font-size:15px" class="fa">&#xf067;</i> Add Logo</a>
-        </div>
+
     </div>
- 
-</div>
-<div class="container" >
-    <table id="table1" class="table table-bordered">
-        <thead>
-            <tr>
-                <th>S.L</th>
-                <th>Title</th>
-                <th>Logo</th>
-                <th>Status</th>
-                <th>Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($logo as $logos)
-            <tr>
-                <td>{{$loop->index+1}}</td>
-                <td>{{$logos->name}}</td>
-                <td><img src="{{asset('LogoImage/'.$logos->image)}}" alt="" style="width: 50px;"></td>
-                <td>
-                    @if($logos->status = '1')
-                    Published
-                    @endif
-                    @if($logos->status = '0')
-                    Unpublished
-                    @endif
-                </td>
-                <td>
-                    <a href="javascript:void(0)" class="btn btn-info" onclick="updateBlog({{ $logos->id }})">Edit</a>
-                    <a href="javascript:void(0)" class="btn btn-danger" onclick="deletelogos({{ $logos->id }})">delete</a>
-                </td>
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
+    <div class="container" >
+        <table id="table1" class="table table-bordered">
+            <thead>
+                <tr>
+                    <th style="width:10%; text-align:center;">S.L</th>
+                    <th style="width:60%; text-align:center;">Title</th>
+                    <th style="width:10%; text-align:center;">Logo</th>
+                    <th style="width:10%; text-align:center;">Status</th>
+                    <th style="width:10%; text-align:center;">Action</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach($logo as $logos)
+                <tr>
+                    <td>{{$loop->index+1}}</td>
+                    <td>{{$logos->name}}</td>
+                    <td><img src="{{asset('LogoImage/'.$logos->image)}}" alt="" style="width: 50px;"></td>
+                    <td>
+                        @if($logos->status = '1')
+                        Published
+                        @endif
+                        @if($logos->status = '0')
+                        Unpublished
+                        @endif
+                    </td>
+                    <td>
+                        <div class="dropdown">
+                            <button class="btn btn-secondary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                Action
+                            </button>
+                            <div class="dropdown-menu pl-1 mt-2"  aria-labelledby="dropdownMenuButton">
+                                <a href="javascript:void(0)" class="btn btn-info " style="width: 47%;" onclick="updateBlog({{ $logos->id }})" >Edit</a>
+                                <a href="javascript:void(0)" class="btn btn-danger" style="width: 47%;" onclick="deletelogos({{ $logos->id }})">delete</a>
+                            </div>
+                        </div>
+
+                    </td>
+                </tr>
+                @endforeach
+            </tbody>
+        </table>
+    </div>
 </div>
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.0/jquery.min.js"></script>
 <script type="text/javascript" src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
@@ -66,7 +74,7 @@
 </script>
 <script>
     $('#table1').DataTable({
-        "pageLength": 3
+        "pageLength": 10
     });
 </script>
 @endsection
